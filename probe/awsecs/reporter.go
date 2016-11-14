4be53626a8258f47e0007c0da745dc444cc5c41a
Mike Lang <mikelang3000@gmail.com>
2016-11-14 14:35:18 -0800

    wip:

--- probe/awsecs/reporter.go
+++ probe/awsecs/reporter.go
@@ -4,13 +4,62 @@ package awsecs
 import (
 )
 
-struct labelInfo {
-	containerID string
-	cluster string
-	taskArn string
+struct taskInfo {
+	containerIDs []string
 	family string
 }
 
-func getLabelInfos() []labelInfo {
-	// TODO get labels for all containers and look for com.amazonaws.ecs.{cluster,task-arn,task-definition-family}
+// return map from cluster to map of task arns to task infos
+func getLabelInfo(rpt report.Report) map[string][string]taskInfo {
+	results := make(map[string][string]taskInfo)
+	for nodeID, node := range rpt.Container.Nodes {
+
+		taskArn, taskArnOk := node.Latest.Lookup(docker.LabelPrefix + "com.amazonaws.ecs.task-arn")
+		cluster, clusterOk := node.Latest.Lookup(docker.LabelPrefix + "com.amazonaws.ecs.cluster")
+		family, familyOk := node.Latest.Lookup(docker.LabelPrefix + "com.amazonaws.ecs.task-definition-family")
+
+		if taskArnOk && clusterOk && familyOk {
+			taskMap, ok := results[cluster]
+			if !ok {
+				taskMap = make(map[string]taskInfo)
+				results[cluster] = taskMap
+			}
+
+			task, ok := results[taskArn]
+			if !ok {
+				task = taskInfo{containerIDs: make([]string), family: family}
+				results[taskArn] = task
+			}
+
+			task.containerIDs = append(task.containerIDs, nodeID)
+		}
+	}
+	return results
+}
+
+// implements Tagger
+struct Reporter {
+}
+
+func (r *Reporter) Tag(rpt report.Report) (report.Report, error) {
+
+	clusterMap := getLabelInfo()
+
+	for cluster, taskMap := range clusterMap {
+
+		taskServices := newClient(cluster).getTaskServices()
+
+		for _, serviceName := range taskServices {
+			// TODO create a ecs service node
+		}
+
+		for taskArn, info := range taskMap {
+			// TODO create task node with family
+			for _, containerID := range info.containerIDs {
+				// TODO set task node as parent of container
+			}
+		}
+
+	}
+
 }
